d6557af0c0587e39decad4e4e6c89f16935b087e
Azizullah Darmani <40940151-haidarira69@users.noreply.replit.com>
2025-03-31 20:45:25 +0000

    Restored to '260f28420473323046ed79862f2f7afe1345e5b2'
    Replit-Restored-To: 260f28420473323046ed79862f2f7afe1345e5b2

--- django_project/urls.py
+++ django_project/urls.py
@@ -22,5 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('hotels/', include('hotels.urls', namespace='hotels')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
